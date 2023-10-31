6ff9bcb5861c2747d5f9b6ce7f246ac08ce9bc2f
Yannick Carissan <ycarissan@gmail.com>
2023-10-31 11:43:40 +0100

    yeah

--- get_barycenter.py
+++ get_barycenter.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-filename=str(sys.argv[0])
-listatoms=[int(a) for a in sys.argv[1:]]
+filename=str(sys.argv[1])
+listatoms=[int(a) for a in sys.argv[2:]]
 print('Fichier xyz : ', filename)
 print("Atomes : ", listatoms)
 
 fio = open(filename)
 
-il=0
-for l in fio.getlines():
+il=-2
+x=0
+y=0
+z=0
+for l in fio.readlines():
     il=il+1
     if il in listatoms:
-        x = l.split(l)[1]
-        y = l.split(l)[2]
-        z = l.split(l)[3]
+        x = x + float(l.split()[1])
+        y = y + float(l.split()[2])
+        z = z + float(l.split()[3])
 
 print(x/len(listatoms),y/len(listatoms),z/len(listatoms))
